8caee23f03b30bc5150ba4671903ea0297101aa0
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-10-31 23:01:20 +0100

    clang: precompiled header and rc file

---
+++ src/pch.h
@@ -0,0 +1,16 @@
+#ifndef __PCH_H__
+#     define __PCH_H__
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+#include <boost/utility.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/format.hpp>
+#include <boost/shared_ptr.hpp>
+
+#endif /* __PCH_H__ */
