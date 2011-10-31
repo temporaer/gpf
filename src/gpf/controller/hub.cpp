f8df933b79f72158db62f9e6277108a8384d8842
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-10-31 23:05:33 +0100

    implement hub a bit more (most dispatchers still missing)

--- src/gpf/controller/hub.cpp
+++ src/gpf/controller/hub.cpp
@@ -1,19 +1,148 @@
+#include <boost/bind.hpp>
 #include <boost/format.hpp>
 
 #include <glog/logging.h>
 
 #include <gpf/controller/hub.hpp>
+#include <gpf/util/message_util.hpp>
 
 using namespace gpf;
 using boost::format;
 
 
-hub::hub()
+// Hub constructor
+hub::hub(
+				zmq_reactor::reactor<>& m_loop,
+				boost::shared_ptr<zmq::socket_t> monitor,
+				boost::shared_ptr<zmq::socket_t> query,
+				boost::shared_ptr<zmq::socket_t> notifier,
+				boost::shared_ptr<zmq::socket_t> resubmit,
+				engine_info ei,
+				client_info ci
+		)
 {
+
+	m_loop.add(*query,    ZMQ_POLLIN, boost::bind(&hub::dispatch_query,this, _1));
+	m_loop.add(*monitor,  ZMQ_POLLIN, boost::bind(&hub::dispatch_monitor_traffic,this, _1));
+	m_loop.add(*resubmit, ZMQ_POLLIN, [=](zmq::socket_t&){});
+
+	m_monitor_handlers["in"]         = &hub::save_queue_request;
+	m_monitor_handlers["out"]        = &hub::save_queue_result;
+	m_monitor_handlers["intask"]     = &hub::save_task_request;
+	m_monitor_handlers["outtask"]    = &hub::save_task_result;
+	m_monitor_handlers["tracktask"]  = &hub::save_task_destination;
+	m_monitor_handlers["incontrol"]  = &hub::nop;
+	m_monitor_handlers["outcontrol"] = &hub::nop;
+	m_monitor_handlers["iopub"]      = &hub::save_iopub_message;
+
+	m_query_handlers["queue_request"]          = &hub::queue_status;
+	m_query_handlers["result_request"]         = &hub::get_results;
+	m_query_handlers["history_request"]        = &hub::get_history;
+	m_query_handlers["db_request"]             = &hub::db_query;
+	m_query_handlers["purge_request"]          = &hub::purge_results;
+	m_query_handlers["load_request"]           = &hub::check_load;
+	m_query_handlers["resubmit_request"]       = &hub::resubmit_task;
+	m_query_handlers["shutdown_request"]       = &hub::shutdown_request;
+	m_query_handlers["registration_request"]   = &hub::register_engine;
+	m_query_handlers["unregistration_request"] = &hub::unregister_engine;
+	m_query_handlers["connection_request"]     = &hub::connection_request;
+
+	LOG(INFO)<<"hub::created hub";
 }
+
+void hub::dispatch_monitor_traffic(zmq::socket_t& s){
+	// all ME and Task queue messages come through here, as well as
+	// IOPub traffic.
+	ZmqMessage::Incoming<ZmqMessage::SimpleRouting> incoming(s);
+	incoming.receive_all();
+	std::string type = ZmqMessage::get<std::string>(incoming[0]);
+	DLOG(INFO) << "Monitor traffic : "<< ZmqMessage::get<std::string>(incoming[0]);
+	
+	int msg_start;
+	std::vector<std::string> idents;
+	util::feed_identities(idents,msg_start,incoming);
+	if(idents.size()==0)
+	{
+		LOG(ERROR) << "Bad Monitor Message!";
+		return;
+	}
+
+	std::map<std::string,monitor_handler_t>::iterator handler = m_monitor_handlers.find(type);
+	if(handler == m_monitor_handlers.end()){
+		LOG(ERROR) << "Invalid monitor topic: "<<type;
+		return;
+	}
+	(this->*(handler->second))(idents, msg_start, incoming);
+}
+void hub::dispatch_query(zmq::socket_t&s){
+	// Route registration requests and queries from clients.
+	ZmqMessage::Incoming<ZmqMessage::SimpleRouting> incoming(s);
+	std::vector<std::string> idents;
+	int msg_start;
+	util::feed_identities(idents,msg_start,incoming);
+	if(idents.size()==0){
+		LOG(ERROR) << "Bad Query Message! ";
+		return;
+	}
+
+	message msg;
+	try{
+		util::deserialize(msg, msg_start, incoming);
+	}catch(std::exception e){
+		LOG(ERROR) << "Bad Query Mesage: Unserialize failed!\n"<<e.what();
+	}
+	
+	
+	std::map<std::string,query_handler_t>::iterator handler = m_query_handlers.find(msg.msg_type);
+	if(handler == m_query_handlers.end()){
+		LOG(ERROR) << "Bad Message Type: "<<msg.msg_type;
+		return;
+	}
+	(this->*(handler->second))(idents, msg_start, incoming);
+
+}
+
 hub::~hub()
 {
 }
+
+void hub::nop(std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	// TODO: should this func recv the msg nevertheless?
+}
+void hub::save_queue_request(std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	
+}
+void hub::save_queue_result (std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	
+}
+void hub::save_task_request(std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	
+}
+void hub::save_task_result(std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	
+}
+void hub::save_task_destination(std::vector<std::string>&, int msg_start, incoming_msg_t&){
+	
+}
+
+void hub::shutdown_request(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::_shutdown(){}
+void hub::check_load(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::queue_status(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::purge_results(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::resubmit_task(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::get_results(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::db_query(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::get_history(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+
+void hub::save_iopub_message(std::vector<std::string>&, int msg_start, incoming_msg_t&){}
+void hub::connection_request(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::register_engine(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+void hub::unregister_engine(std::vector<std::string>&,int msg_start,incoming_msg_t&){}
+
+ /**********************************
+  *         Hub Factory
+  **********************************/
 
 hub_factory::hub_factory(int startport)
 :m_portpool(startport),
@@ -57,6 +186,8 @@ hub_factory::transport(const std::string& transport){
 
 boost::shared_ptr<hub>
 hub_factory::get(){
+	typedef boost::shared_ptr<zmq::socket_t> zmq_socket;
+
 	std::string client_iface = str(format("%s://%s:")
 			%m_client_transport
 			%m_client_ip) + "%i";
@@ -65,35 +196,38 @@ hub_factory::get(){
 			%m_engine_transport
 			%m_engine_ip) + "%i";
 
-	zmq::socket_t q (m_ctx, ZMQ_ROUTER);
-	q.bind((format(client_iface)%m_reg_port).str().c_str());
+	zmq_socket q( new zmq::socket_t(m_ctx, ZMQ_ROUTER) );
+	q->bind((format(client_iface)%m_reg_port).str().c_str());
 
 	LOG(INFO) << "Hub listening on `"<<format(client_iface)%m_reg_port
 		<< "' for registration.";
 
 	if(m_client_ip != m_engine_ip){
-		q.bind((format(engine_iface)%m_reg_port).str().c_str());
+		q->bind((format(engine_iface)%m_reg_port).str().c_str());
 		LOG(INFO) << "Hub listening on `"<<format(engine_iface)%m_reg_port
 			<< "' for registration.";
 	}
 
+
 	// Heartbeat
-	zmq::socket_t hpub(m_ctx, ZMQ_PUB);
-	hpub.bind(str(format(engine_iface)%m_hb_ports[0]).c_str());
-	zmq::socket_t hrep(m_ctx, ZMQ_ROUTER);
-	hrep.bind(str(format(engine_iface)%m_hb_ports[1]).c_str());
+	zmq_socket hpub (new zmq::socket_t(m_ctx, ZMQ_PUB) );
+	hpub->bind(str(format(engine_iface)%m_hb_ports[0]).c_str());
+	zmq_socket hrep (new zmq::socket_t(m_ctx, ZMQ_ROUTER) );
+	hrep->bind(str(format(engine_iface)%m_hb_ports[1]).c_str());
+
+	m_heartmonitor.reset(new heartmonitor(m_reactor, hpub, hrep));
 	
 
 	// client connections
 	// notifier socket
-	zmq::socket_t n(m_ctx, ZMQ_PUB);
-	n.bind((format(client_iface)%m_notifier_port).str().c_str());
+	zmq_socket n( new zmq::socket_t (m_ctx, ZMQ_PUB) );
+	n->bind((format(client_iface)%m_notifier_port).str().c_str());
 
 	// build and launch queues
-	zmq::socket_t sub(m_ctx, ZMQ_SUB);
-	sub.setsockopt(ZMQ_SUBSCRIBE, "", 0);
-	sub.bind(m_monitor_url.c_str());
-	sub.bind("inproc://monitor");
+	zmq_socket sub( new zmq::socket_t(m_ctx, ZMQ_SUB) );
+	sub->setsockopt(ZMQ_SUBSCRIBE, "", 0);
+	sub->bind(m_monitor_url.c_str());
+	sub->bind("inproc://monitor");
 
 	// build info structs
 	client_info ci;
@@ -127,9 +261,11 @@ hub_factory::get(){
 		<< "     task_scheme    " <<ci.task_scheme<<std::endl;
 
 	// resubmit stream
-	zmq::socket_t r(m_ctx, ZMQ_DEALER);
+	zmq_socket r(new zmq::socket_t(m_ctx, ZMQ_DEALER));
 	const char* session_name = "session-name";
-	r.setsockopt(ZMQ_IDENTITY, session_name, strlen(session_name));
-	r.connect(ci.task.c_str());
-	return boost::shared_ptr<hub>();
+	r->setsockopt(ZMQ_IDENTITY, session_name, strlen(session_name));
+	r->connect(ci.task.c_str());
+	boost::shared_ptr<hub> H( new hub(m_reactor, sub, q, n, r,ei,ci));
+
+	return H;
 }
--- src/gpf/controller/hub.hpp
+++ src/gpf/controller/hub.hpp
@@ -1,35 +1,21 @@
 #ifndef __CONTROLLER_HPP__
 #     define __CONTROLLER_HPP__
 
+#include <boost/function.hpp>
 #include <boost/utility.hpp>
 #include <boost/shared_ptr.hpp>
 #include <string>
+#include <map>
 
 #include <zmq.hpp>
 #include <zmq_utils.h> 
 #include <gpf/util/portpool.hpp>
+#include <gpf/controller/heartmonitor.hpp>
+#include <zmq-poll-wrapper/reactor.hpp>
+#include <gpf/util/zmqmessage.hpp>
+#include <gpf/message.hpp>
 
 namespace gpf{
-
-	class hub
-	: boost::noncopyable
-	{
-	public:
-		hub( );
-		~hub();
-		
-	private:
-		// session: Session object
-		// queue: ZMQStream for monitoring the command queue (SUB)
-		// query: ZMQStream for engine registration and client queries requests (XREP)
-		// heartbeat: HeartMonitor object checking the pulse of the engines
-		// notifier: ZMQStream for broadcasting engine registration changes (PUB)
-		// engine_info: dict of zmq connection information for engines to connect
-		// to the queues.
-		// client_info: dict of zmq connection information for engines to connect
-		// to the queues.
-	};
-
 	struct engine_info
 	{
 		std::string control;
@@ -48,6 +34,74 @@ namespace gpf{
 		std::string iopub;
 		std::string notification;
 	};
+
+
+	class hub
+	: boost::noncopyable
+	{
+	public:
+		hub( 
+				zmq_reactor::reactor<>& m_loop,
+				boost::shared_ptr<zmq::socket_t> monitor,
+				boost::shared_ptr<zmq::socket_t> query,
+				boost::shared_ptr<zmq::socket_t> notifier,
+				boost::shared_ptr<zmq::socket_t> resubmit,
+				engine_info ei,
+				client_info ci
+		);
+		~hub();
+		
+	private:
+		void dispatch_monitor_traffic(zmq::socket_t&); // ME, IOPub and Task queue messages
+		void dispatch_query(zmq::socket_t&); // requests from client
+
+		// Heartbeat
+		void handle_new_heart(zmq::socket_t&);
+		void handle_heart_failure(zmq::socket_t&);
+
+		typedef ZmqMessage::Incoming<ZmqMessage::SimpleRouting> incoming_msg_t;
+
+		// MUX Queue Traffic
+		void nop(std::vector<std::string>&, int msg_start, incoming_msg_t&); // TODO: should this func recv the msg nevertheless?
+		void save_queue_request(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+		void save_queue_result (std::vector<std::string>&, int msg_start, incoming_msg_t&);
+
+		// Task Queue Traffic
+		void save_task_request(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+		void save_task_result(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+		void save_task_destination(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+
+		// IOPub traffic
+		void save_iopub_message(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+
+		// registration requests
+		void connection_request(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void register_engine(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void unregister_engine(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void _handle_stranded_msgs(const std::string& eid, const std::string& uuid);
+		void finish_registration(const std::string& heart);
+		void _purge_stalled_registration(const std::string& heart);
+
+		// client requests
+		void shutdown_request(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void _shutdown();
+		void check_load(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void queue_status(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void purge_results(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void resubmit_task(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		//void _extract_record(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void get_results(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void db_query(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		void get_history(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+
+		typedef void (hub::*monitor_handler_t)(std::vector<std::string>&, int msg_start, incoming_msg_t&);
+		typedef void (hub::*query_handler_t)(std::vector<std::string>&,int msg_start,incoming_msg_t&);
+		std::map<std::string,monitor_handler_t> m_monitor_handlers;
+		std::map<std::string,query_handler_t>   m_query_handlers;
+
+
+	};
+
 
 	class hub_factory{
 		public:
@@ -88,10 +142,12 @@ namespace gpf{
 			port_t m_mon_port;      // Monitor (UB) port for queue traffic
 			port_t m_notifier_port; // PUB port for sending engine status notifications
 			port_t m_reg_port;      // For registration
-
+			
 			// heart monitor
+			boost::shared_ptr<heartmonitor> m_heartmonitor;
 
-
+			// reactor
+			zmq_reactor::reactor<> m_reactor;
 	};
 	
 };
