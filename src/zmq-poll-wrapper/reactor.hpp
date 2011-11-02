eae029f44603829ef2928efbd737916085f83884
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-11-02 15:35:25 +0100

    clean up reactor, add timeout callbacks

--- src/zmq-poll-wrapper/reactor.hpp
+++ src/zmq-poll-wrapper/reactor.hpp
@@ -35,15 +35,12 @@
 #include <vector>
 #include <functional>
 #include <boost/foreach.hpp>
-#include "OneMethodOnePointerParamInterface.hpp"
 #include <algorithm>
 #include <glog/logging.h>
+#include "timer.hpp"
 
 namespace zmq_reactor
 {
-
-	typedef OneMthdOneParamPtrInterface<zmq::socket_t> PollEventInterface;
-
 
 	template <class T>
 		inline
@@ -58,97 +55,71 @@ namespace zmq_reactor
 			item.socket = v;
 		}
 
-	template <class T, class V, class R>
-		inline
-		void trait(T& cb, V* v, R* r)
-		{
-			cb(*v, r); 
-		};
-	template <class T, class V>
-		inline
-		void trait(T& cb, V* v, void* r)
-		{
-			cb(*v); 
-		};
 
-
-	template <class SOCKET = zmq::socket_t>
 		struct reactor
 		{
-			typedef boost::function<void (zmq::socket_t&)>  callback_t;
+			typedef zmq::socket_t   socket_t;
+			typedef boost::function<void (socket_t&)>        socket_activity_callback_t;
+			typedef boost::function<void (reactor*)>         timeout_callback_t;
 
-			bool add(SOCKET& v, short event, callback_t cb, bool checkIfSocketAddedTwice = true)
+			void add(const gpf::deadline_timer& dt){
+			       	m_timer_queue.queue.push(dt);
+			}
+			bool add(socket_t& v, short event, socket_activity_callback_t cb, bool checkIfSocketAddedTwice = true)
 			{
 				zmq_pollitem_t item = {0,0,0,0};
 				set(v, item);
 				item.events = event;
 				return addImpl(item, v, cb, checkIfSocketAddedTwice);
 			}
-			bool add(short event, callback_t cb){
+			bool add(short event, socket_activity_callback_t cb){
 				if(event == ZMQ_POLLIN)
 					m_pre_receive_callbacks.push_back(cb);
 				return true;
 			}
-			bool remove(SOCKET& v)
+			bool remove(socket_t& v)
 			{
 				return removeImpl(v);
 			}
-			template <typename STATE>
-			int operator()(STATE* state = 0, int timeout = -1)
+			void shutdown(){ m_stop_requested=true; }
+			void run(long timeout = -1){
+				m_stop_requested = false;
+				while(!m_stop_requested && operator()(timeout)>=0);
+			}
+			int operator()(long timeout = -1)
 			{
+				if(timeout < 0)
+					timeout = m_timer_queue.next_timeout();
+				else
+					timeout = std::min(timeout, m_timer_queue.next_timeout());
 				int ret = zmq::poll (&items_[0], items_.size(), timeout);
-				if ((ret == 0) || (ret == -1) )
+				if( ret==0 ){
+					// timeout
+					m_timer_queue.fire_due(this);
 					return ret;
+				}else if(ret < 0){
+					// error
+					throw std::runtime_error("zmq::poll returned an error");
+				}
+				// socket fired
 
-				typename std::vector<SOCKET*>::iterator skit = socks_.begin();
-				std::vector<callback_t>::iterator cit        = m_callbacks.begin(); // outside "for" to keep VS2010 happy
+				std::vector<socket_t*>::iterator skit = socks_.begin();
+				std::vector<socket_activity_callback_t>::iterator cit        = m_callbacks.begin(); // outside "for" to keep VS2010 happy
 				std::vector<zmq_pollitem_t>::iterator it     = items_.begin();      // keep clang happy
 				for (; it != items_.end(); ++it, ++cit, ++skit)
 				{
 					if (it->revents & it->events)
 					{	
-						callback_t& cb = *cit;
-						SOCKET* v    = *skit;
+						socket_activity_callback_t& cb = *cit;
+						socket_t* v    = *skit;
 
-						BOOST_FOREACH(callback_t& cb, m_pre_receive_callbacks){
-							trait(cb,v,state);
+						BOOST_FOREACH(socket_activity_callback_t& cb, m_pre_receive_callbacks){
+							cb(*v);
 						}
-						trait(cb,v, state);
+						cb(*v);
 					}
 				}
 				return ret;
-			}
-			int operator()( int timeout = -1) //  overload to keep VS2010 happy - no defaults for functions
-			{
-				return this->operator()((void*)0, timeout);
-			}
-
-			template <typename STATE>
-				int run(	STATE* state = 0,  int timeout = -1, 
-						int (*begin)(STATE*, reactor&, int&) = 0, 
-						int (*end)(STATE*, reactor&, int&, int) = 0)
-				{
-					while (1)
-					{
-						int tmout = timeout;
-						if (begin)
-						{
-							int ret = begin(state, *this, tmout);
-							if (ret) return ret;
-						}
-						int ret = this->operator()(state, tmout);
-						if (ret == -1)
-							return ret;
-						if (end)
-						{
-							int ret = end(state, *this, tmout, ret);
-							if (ret) return ret;
-						}
-					}
-				}
-			int run(int timeout = -1) //  overload to keep VS2010 happy - no defaults for functions
-			{
-				return run((void*)0, timeout, 0, 0);
 			}
 
 			private:
@@ -158,7 +129,8 @@ namespace zmq_reactor
 					typename std::vector<TT*>::const_iterator it = std::find( k.begin(), k.end(), t );
 					return it == k.end() ? -1 : it - k.begin();
 				}
-			bool addImpl(zmq_pollitem_t& item, SOCKET& v, callback_t cb, bool checkIfSocketAddedTwice)
+
+			bool addImpl(zmq_pollitem_t& item, socket_t& v, socket_activity_callback_t cb, bool checkIfSocketAddedTwice)
 			{
 				//if added twice it hangs
 				if (checkIfSocketAddedTwice && (getIndex(socks_, &v) > -1) )
@@ -175,7 +147,7 @@ namespace zmq_reactor
 					k.erase(k.begin() + pos);
 				}
 
-			bool removeImpl(SOCKET& v)
+			bool removeImpl(socket_t& v)
 			{
 				int pos = getIndex(socks_, &v);
 				if (pos == -1)
@@ -187,11 +159,13 @@ namespace zmq_reactor
 			}
 			private:
 			std::vector<zmq_pollitem_t> items_;
-			std::vector<callback_t>       m_callbacks;
+			std::vector<socket_activity_callback_t>       m_callbacks;
 
-			// TODO: add timeouts callbacks
-			std::vector<callback_t>       m_pre_receive_callbacks;
-			std::vector<SOCKET*>        socks_;
+			bool                                          m_stop_requested;
+			gpf::timer_queue                              m_timer_queue;
+
+			std::vector<socket_activity_callback_t>       m_pre_receive_callbacks;
+			std::vector<socket_t*>        socks_;
 		};
 
 }
