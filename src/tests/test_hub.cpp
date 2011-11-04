729570c7d96e9262cd18db3a4092dc45712eeabf
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-11-04 16:21:19 +0100

    init engine test (well... it will be a test eventually...)

--- src/tests/test_hub.cpp
+++ src/tests/test_hub.cpp
@@ -1,10 +1,21 @@
 #include <gtest/gtest.h>
 
 #include <gpf/controller/hub.hpp>
+#include <gpf/engine/engine.hpp>
 
-TEST(hub_test, init){
+TEST(hub_test, hub_init){
 	gpf::hub_factory hf(5000);
 	hf.ip("127.0.0.1").transport("tcp");
 	hf.get();
 }
 
+TEST(hub_test, engine_init){
+	gpf::hub_factory hf(5000);
+	hf.ip("127.0.0.1").transport("tcp");
+
+	boost::shared_ptr<gpf::hub> p( hf.get() );
+	gpf::engine_info ei = p->get_engine_info();
+
+	zmq::context_t ctx(1);
+	gpf::engine engine(ctx);
+}
