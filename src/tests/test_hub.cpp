6cd25d9a879a9ee04821012956fed826f6a75ae6
Hannes <schulz@ais.uni-bonn.de>
2011-11-06 19:27:35 +0100

    hub test: check registration and unregistration of engines (automatically now. sigh.)

--- src/tests/test_hub.cpp
+++ src/tests/test_hub.cpp
@@ -11,24 +11,68 @@ TEST(hub_test, hub_init){
 	hf.get();
 }
 
+template<class T>
+void schedule_reactor_shutdown(int ms, T& t){
+	t.get_loop().add(gpf::deadline_timer(boost::posix_time::milliseconds(ms),
+				boost::bind(&zmq_reactor::reactor::shutdown,&t.get_loop())));
+}
+
+template<class T>
+void schedule_obj_shutdown(int ms, T& t){
+	t.get_loop().add(gpf::deadline_timer(boost::posix_time::milliseconds(ms),
+				boost::bind(&T::shutdown,&t)));
+}
+
 TEST(hub_test, engine_init){
 	gpf::hub_factory hf(5000);
-	hf.ip("127.0.0.1").transport("tcp");
-	int timeout_secs = 5;
+	hf.ip("127.0.0.1").transport("tcp").hm_interval(200);
 
 	boost::shared_ptr<gpf::hub> hub = hf.get();
-	// schedule shutdown in a second
-	hub->get_loop().add(gpf::deadline_timer(boost::posix_time::seconds(timeout_secs),
-				boost::bind(&zmq_reactor::reactor::shutdown,&hub->get_loop())));
 
 	zmq::context_t ctx(1);
 	gpf::engine engine(ctx);
-	engine.get_loop().add(gpf::deadline_timer(boost::posix_time::seconds(timeout_secs),
-	                        boost::bind(&zmq_reactor::reactor::shutdown,&engine.get_loop())));
+	engine.provide_service("test-service1");
+	engine.provide_service("test-service2");
+
+	schedule_reactor_shutdown(500, *hub);
+	schedule_reactor_shutdown(500, engine);
 
 	boost::thread engine_thread([&](){engine.run("engine0", hub->get_engine_info());});
 
-
 	hub->run();
 	engine_thread.join();
+
+	/***
+	 * the engine should now be registered with the hub
+	 ***/
+	EXPECT_TRUE( engine.registered() );
+	EXPECT_ANY_THROW( hub->get_engine("dummy-engine-queue") );
+	EXPECT_NO_THROW(hub->get_engine("engine0-queue"));
+
+	EXPECT_EQ(hub->get_num_engines(), 1);
+	EXPECT_EQ(hub->get_engine("engine0-queue").services.size(), 2);
+	EXPECT_EQ(hub->get_engine("engine0-queue").services[0], "test-service1");
+	EXPECT_EQ(hub->get_engine("engine0-queue").services[1], "test-service2");
+
+
+	/****
+	 * continue execution
+	 ****/
+
+	schedule_reactor_shutdown(200, *hub);
+	schedule_obj_shutdown(100, engine);
+
+	boost::thread engine_thread2([&](){engine.get_loop().run();});
+
+	hub->run();
+	engine_thread2.join();
+
+	/***
+	 * test unregistration of engine
+	 */
+
+	EXPECT_ANY_THROW(hub->get_engine("engine0-queue"));
+
+	EXPECT_EQ(hub->get_num_engines(), 0);
+
 }
