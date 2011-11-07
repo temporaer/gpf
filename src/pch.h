df448620a59769f44fceca6d6104a817f7b6dc38
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-11-07 11:56:01 +0100

    update precompiled clang header

--- src/pch.h
+++ src/pch.h
@@ -1,10 +1,13 @@
 #ifndef __PCH_H__
 #     define __PCH_H__
 
-#include <vector>
-#include <iostream>
 #include <string>
+#include <iostream>
+
+#include <vector>
 #include <map>
+#include <set>
+
 #include <algorithm>
 
 #include <boost/utility.hpp>
@@ -12,5 +15,13 @@
 #include <boost/bind.hpp>
 #include <boost/format.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/uuid/uuid.hpp>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
+
+//#include <boost/exception/all.hpp>
 
 #endif /* __PCH_H__ */
